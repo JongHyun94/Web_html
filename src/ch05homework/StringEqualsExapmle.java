196087af3a0b2a3be77da66dd95ea6becd122f51
JongHyun94 <ant9406@naver.com>
2021-02-17 14:26:05 +0900

    코드 추가

--- src/ch05homework/StringEqualsExapmle.java
+++ src/ch05homework/StringEqualsExapmle.java
@@ -13,6 +13,10 @@ public class StringEqualsExapmle {
 			System.out.println("strVar1과 strVar2는 참조가 다름");
 		}
 		
+		if(strVar1.equals(strVar2)) {
+			System.out.println("strVar1과 strVar2는 문자열이 같음");
+		}
+		
 		
 	}
 
